2f00ccdc01f9026dc9fd152fe0090a2eacd6c592
rgulles <gulles.ronian@gmail.com>
2026-04-27 20:48:47 +0800

    Add Settings class and refactor Worddle structure

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from settings import Settings
 
 class Worddle:
     def __init__(self, master):
         self.master = master
+        self.settings = Settings()
 
-        self.master.title('Wordle')
-        self.master.geometry('500x700')
+        self.master.title(self.settings.title)
+        self.master.geometry(f'{self.settings.screen_width}x{self.settings.screen_height}')
 
 if __name__ == "__main__":
     root = Tk()
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings: 
+    def __init__(self):
+
+        # Screen
+        self.title = "Wordle"
+        self.screen_width = 500
+        self.screen_height = 700
